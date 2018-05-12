37fd8f2b2af1f9523615bb5198b65bd511e0a8f7
Matthew Lewis <mel_long@live.com>
2018-05-12 18:12:46 +0100

    Adding scripts to display Stats about my Tilemaps in the Tilemaps inspector. Added 'Rewired' to DTValidator's ValidatorIgnoredNamespace.

---
+++ monsterBattleMini/Assets/Scripts/TileMap/TileMapStats.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class TileMapStats:MonoBehaviour {
+    public void showStats() {
+        Tilemap tilemap = GetComponent<Tilemap>();
+
+        Vector3Int tilemapSize = tilemap.size;
+        Vector3Int tilemapEditorPreviewSize = tilemap.editorPreviewSize;
+        BoundsInt tilemapBounds = tilemap.cellBounds;
+
+        Debug.Log("TilemapSize: " + tilemapSize + " | TilemapEditorPreviewSize: " + tilemapEditorPreviewSize);
+        Debug.Log("TilemapBounds: " + tilemapBounds);
+    }
+}
