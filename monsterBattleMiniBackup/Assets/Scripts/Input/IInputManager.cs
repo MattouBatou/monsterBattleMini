8cf67eed08604c9544853f45e8177eae1455e87c
Matthew Lewis <mel_long@live.com>
2018-05-12 15:45:34 +0100

    Adding backup project with 2 players, 2 follow cams split screen and some tiles placed. This works in 2018.1.0f2.

---
+++ monsterBattleMiniBackup/Assets/Scripts/Input/IInputManager.cs
@@ -0,0 +1,7 @@
+﻿public interface IInputManager {
+    bool isEnabled { get; set; }
+    float GetAxis(int playerId, InputAction action);
+    bool GetButton(int playerId, InputAction action);
+    bool GetButtonDown(int playerId, InputAction action);
+    bool GetButtonUp(int playerId, InputAction action);
+};
